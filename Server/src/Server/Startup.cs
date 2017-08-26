aa5ba71646f875351d33d654531bd43357bceef5
Anton Anikeev <000job00@gmail.com>
2017-08-26 14:36:15 +0300

    Setup WebApi and test endpoint(controller) ValuesController for checking it

---
+++ Server/src/Server/Startup.cs
@@ -0,0 +1,83 @@
+﻿using System.ComponentModel;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Controllers;
+using Microsoft.AspNetCore.Mvc.ViewComponents;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Server.Services;
+using Swashbuckle.AspNetCore.Swagger;
+
+using SimpleInjector;
+using SimpleInjector.Lifestyles;
+using SimpleInjector.Integration.AspNetCore;
+using SimpleInjector.Integration.AspNetCore.Mvc;
+
+namespace Server
+{
+    public class Startup
+    {
+        private Container container = new Container();
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new Info { Title = "My API", Version = "v1" });
+            });
+
+            //Add simple injector resolver 
+            services.AddSingleton<IControllerActivator>(new SimpleInjectorControllerActivator(container));
+            services.UseSimpleInjectorAspNetRequestScoping(container);
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        {
+            loggerFactory.AddConsole();
+
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            InitializeContainer(app);
+            container.Verify();
+
+            // Add ioc middleware
+            //Add swagger
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+            });
+
+            //Setup WebApi
+            app.UseMvcWithDefaultRoute();
+
+            //Add static files 
+            app.UseDefaultFiles();
+            app.UseStaticFiles();
+        }
+
+        private void InitializeContainer(IApplicationBuilder app)
+        {
+            container.Options.DefaultScopedLifestyle = new AsyncScopedLifestyle();
+
+            // Add application presentation components:
+            container.RegisterMvcControllers(app);
+
+            // Add application services. For instance:
+            container.Register<IMyService, MyService>();
+
+            // Cross-wire ASP.NET services (if any). For instance:
+            container.RegisterSingleton(app.ApplicationServices.GetService<ILoggerFactory>());
+
+            // NOTE: Do prevent cross-wired instances as much as possible.
+            // See: https://simpleinjector.org/blog/2016/07/
+        }
+    }
+}
