3569de03e610b931588f393f53175c49e9a37e02
Anton Anikeev <000job00@gmail.com>
2017-10-08 00:45:54 +0300

    Working version of form

--- Server/src/Server/wwwroot/homeex/src/app/create-address-item-form/create-address-item-form.component.ts
+++ Server/src/Server/wwwroot/homeex/src/app/create-address-item-form/create-address-item-form.component.ts
@@ -1,21 +1,19 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm} from '@angular/forms';
+import { AddressItem } from '../address-item/entities/addressItem';
+import { AddressItemsListService } from '../address-items-list.service';
 
 @Component({
   selector: 'app-create-address-item-form',
   templateUrl: './create-address-item-form.component.html',
   styleUrls: ['./create-address-item-form.component.scss']
 })
-export class CreateAddressItemFormComponent implements OnInit {
+export class CreateAddressItemFormComponent{
+  
+  constructor(private _addressItemsListService: AddressItemsListService) { }
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
-  onSubmit(createAddressItemForm: NgForm){
-     
-    console.log(createAddressItemForm);
+  onSubmit(createAddressItemForm: NgForm){     
+    createAddressItemForm.value.streetName
   }
 
 }
